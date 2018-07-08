e43a4496148766685220d22b5d93425a7a588f2d
jeremy <jeremy811@163.com>
2018-07-08 14:11:21 +0800

    add network data

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,53 @@
-Page({
-  data:{
+const weatherMap = {
+  'sunny':'晴天',
+  'cloudy':'多云',
+  'overcast':'阴',
+  'lightrain':'小雨',
+  'heavyrain': '大雨',
+}
+const weatherColorMap = {
+  'sunny': '#cbeeff',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
 
+Page({
+  onPullDownRefresh(){
+    this.getNow(()=>{
+      console.log("stop pull down")
+      wx.stopPullDownRefresh()
+    })
+  },
+  onLoad(){
+      this.getNow()
+  },
+  getNow(callback){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data:{
+        city:"深圳"
+      },
+      success:res => {
+        let result = res.data.result
+        let temp = result.now.temp
+        let weather = result.now.weather
+        console.log(temp, weather)
+        this.setData({
+          nowTemp:temp,
+          nowWeather:weatherMap[weather],
+          nowWeatherBg:'/images/' + weather + '-bg.png'
+        })
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherColorMap[weather]
+        })
+      },
+      complete:()=>{
+        callback && callback()
+      }
+    })
   }
 })
